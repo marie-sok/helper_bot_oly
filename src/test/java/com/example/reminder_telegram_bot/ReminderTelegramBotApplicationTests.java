f143b5de1cc78522186dafaba5f01506c5a1a267
marie-sok <sarrahmcfarlend@icloud.com>
2025-10-18 11:43:10 +0300

    Initial commit

---
+++ src/test/java/com/example/reminder_telegram_bot/ReminderTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reminder_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
