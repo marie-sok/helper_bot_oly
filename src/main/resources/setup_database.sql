5b8dc6e9464f841bcb3993c566adcf8951816c60
Marie Sok <sarrahmcfarlend@icloud.com>
2026-09-05 17:42:15 +0300

    Remove hardcoded database password

--- src/main/resources/setup_database.sql
+++ src/main/resources/setup_database.sql
@@ -1,17 +1,8 @@
-CREATE DATABASE telegram_bot;
+\set ON_ERROR_STOP on
 
+CREATE USER helper_oly WITH PASSWORD :'helper_oly_password';
+CREATE DATABASE telegram_bot OWNER helper_oly;
 
-CREATE USER helper_oly WITH PASSWORD 'mashmallow';
+\connect telegram_bot
 
-
-GRANT ALL PRIVILEGES ON DATABASE telegram_bot TO helper_oly;
-
-
-\c telegram_bot;
-
-GRANT ALL ON SCHEMA public TO helper_oly;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO helper_oly;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO helper_oly;
-
-
-ALTER USER helper_oly CREATEDB;
+GRANT USAGE, CREATE ON SCHEMA public TO helper_oly;
